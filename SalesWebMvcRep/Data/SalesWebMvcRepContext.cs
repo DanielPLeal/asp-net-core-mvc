09f1b0381caf55a96af49fb1b97fea5b4945c296
Daniel Leal <danielpleal@gmail.com>
2019-04-12 00:10:42 -0300

    CRUD Scaffolding - Departments

---
+++ SalesWebMvcRep/Data/SalesWebMvcRepContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvcRep.Controllers;
+
+namespace SalesWebMvcRep.Models
+{
+    public class SalesWebMvcRepContext : DbContext
+    {
+        public SalesWebMvcRepContext (DbContextOptions<SalesWebMvcRepContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvcRep.Controllers.Department> Department { get; set; }
+    }
+}
