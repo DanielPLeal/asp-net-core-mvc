eec29dde147a8d4fb7c3b1ae2254bc78f0b99e2a
Daniel Leal <danielpleal@gmail.com>
2019-04-11 23:46:10 -0300

    Deleting Department view and controller, for user Scaffolding

---
+++ SalesWebMvcRep/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvcRep.Models;
+
+namespace SalesWebMvcRep.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department> ();
+            return View();
+        }
+    }
+}
